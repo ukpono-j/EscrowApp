1d31b256e14f42a2713cdcab4059786f8d70ed44
ukpono-j <ukponoakpan270@gmail.com>
2025-04-16 17:53:33 +0100

    changed inital background mode color

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 // Define custom theme colors (optional)
 const theme = extendTheme({
   config: {
-    initialColorMode: "light", // or 'dark'
+    initialColorMode: "dark", // or 'dark'
     useSystemColorMode: false,
   },
   styles: {
