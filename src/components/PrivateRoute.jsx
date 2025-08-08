e956a01c3fb341e7ea9e9e26cb50e0e57cfdc2db
ukpono-j <ukponoakpan270@gmail.com>
2025-08-08 19:10:23 +0100

    made a slight change to privateRoute file by adding /#/login correctly written since we are using hashrouter

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 // Protects routes requiring authentication by checking for access-token
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('access-token');
-  return token ? children : <Navigate to="/" replace />;
+  return token ? children : <Navigate to="/#/login" replace />;
 };
 
 export default PrivateRoute;
